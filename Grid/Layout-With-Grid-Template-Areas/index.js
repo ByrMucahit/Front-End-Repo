587fbd23becc1b1ffa8157a4fdb4fd9a47b0e518
ByrMucahit <m.mucahitbayar@gmail.com>
2022-03-19 07:57:03 +0300

    Lefts the session that named shortand properties

---
+++ Grid/Layout-With-Grid-Template-Areas/index.js
@@ -0,0 +1,15 @@
+let root = document.documentElement;
+let switcher = document.getElementById("switcher");
+let direction = document.getElementById("direction");
+
+switcher.addEventListener("change", function (evt){
+    root.style.setProperty('--var-writing-mode', evt.target.value);
+});
+
+direction.addEventListener("click", function (evt){
+    if (direction.checked){
+        root.style.setProperty('--var-direction', 'rlt');
+    } else {
+        root.style.setProperty('--var-direction', 'ltr');
+    }
+});
