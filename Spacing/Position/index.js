8b9ceb5ece3e1aa900a3f8d83f72b9ecb8f61f68
ByrMucahit <m.mucahitbayar@gmail.com>
2022-03-20 11:33:27 +0300

    Finishes The Spacing Session

---
+++ Spacing/Position/index.js
@@ -0,0 +1,6 @@
+const select = document.querySelector('select');
+const box = document.querySelector('.box');
+
+select.addEventListener('change', () => {
+    box.style.position = select.value;
+})
